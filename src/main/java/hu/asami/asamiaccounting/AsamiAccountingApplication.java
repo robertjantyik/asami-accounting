da49a24d236c409635e8d0fb4eb554308c5a4610
robert.jantyik <robert.jantyik@dextraline.hu>
2022-08-31 12:27:14 +0200

    init

---
+++ src/main/java/hu/asami/asamiaccounting/AsamiAccountingApplication.java
@@ -0,0 +1,13 @@
+package hu.asami.asamiaccounting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsamiAccountingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsamiAccountingApplication.class, args);
+    }
+
+}
