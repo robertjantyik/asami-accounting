e1a1385fbd73b28fe578d05a97fe9bac2c5be805
robert.jantyik <robert.jantyik@dextraline.hu>
2022-08-31 16:18:26 +0200

    component scan

--- src/main/java/hu/asami/asamiaccounting/AsamiAccountingApplication.java
+++ src/main/java/hu/asami/asamiaccounting/AsamiAccountingApplication.java
@@ -2,8 +2,10 @@ package hu.asami.asamiaccounting;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = { "hu.asami" })
 public class AsamiAccountingApplication {
 
     public static void main(String[] args) {
